ee5c88f693621124a95aeeb1261c91033cda4a54
EDK367 <denilmonterroso22@gmail.com>
2025-04-16 14:37:28 -0600

    Equipos: creacion del templeate de equipos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { RegistrationsComponent } from './features/registrations/registrations.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'registrations', component: RegistrationsComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
---
+++ src/app/features/registrations/registrations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-registrations',
+  imports: [RouterLink],
+  templateUrl: './registrations.component.html',
+  styleUrl: './registrations.component.css'
+})
+export class RegistrationsComponent {
+
+}
