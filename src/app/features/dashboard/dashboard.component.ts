5a0d715ab6c5f9cc3b317b46f01e0746c1477054
EDK367 <denilmonterroso22@gmail.com>
2025-06-01 20:26:01 -0600

    Admin: se modificaron bases de apariencia

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { PanelMenu } from 'primeng/panelmenu';
 import { BadgeModule } from 'primeng/badge';
@@ -10,17 +10,18 @@ import { TournamentService } from '../../services/tournament/tournament.service'
 import { TeamService } from '../../services/team/team.service';
 import { MatchService } from '../../services/match/match.service';
 import { PlayerService } from '../../services/player/player.service';
-
-
+import { filter } from 'rxjs/operators';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, PanelMenu, BadgeModule, Ripple, NgFor, NgIf],
+  imports: [RouterLink, RouterOutlet, BadgeModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
+  name: string = "Campus League";
   activeTournaments: number = 0;
   todaysMatches: number = 0;
   teamsCountR: number = 0;
@@ -29,11 +30,18 @@ export class DashboardComponent implements OnInit {
   activeDropdown: string | null = null;
 
   constructor(
+    private router: Router,
     private tournamentService: TournamentService,
     private matchService: MatchService,
     private teamService: TeamService,
     private playerService: PlayerService
-  ) { }
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.loadName();
+    });
+  }
 
   async ngOnInit() {
     this.loadCountData();
@@ -57,16 +65,16 @@ export class DashboardComponent implements OnInit {
       }).length;
 
       this.teamsCountR = teams
-        .filter(team => team.status?.id !== undefined && [2,10,13].includes(team.status.id))
+        .filter(team => team.status?.id !== undefined && [2, 10, 13].includes(team.status.id))
         .length;
 
       this.teamsCountA = teams
-        .filter(team => team.status?.id !== undefined && [1,3,5,8].includes(team.status.id))
+        .filter(team => team.status?.id !== undefined && [1, 3, 5, 8].includes(team.status.id))
         .length;
 
       this.playersCount = players
-      .filter(player => player.status?.id !== undefined && [1,2,5,8,].includes(player.status.id))
-      .length;
+        .filter(player => player.status?.id !== undefined && [1, 2, 5, 8,].includes(player.status.id))
+        .length;
     } catch (error) {
       console.error('Error loading Data:', error);
       this.activeTournaments = 0;
@@ -74,6 +82,35 @@ export class DashboardComponent implements OnInit {
 
   }
 
+  getName(): string {
+    return this.name;
+  }
+  loadName() {
+    const route = this.router.url;
+
+    switch (route) {
+      case '/dashboard/tournament':
+        this.name = 'Torneos';
+        break;
+      case '/dashboard/matches':
+        this.name = 'Partidos';
+        break;
+      case '/dashboard/referees':
+        this.name = 'Árbitros';
+        break;
+      case '/dashboard/fields':
+        this.name = 'Canchas';
+        break;
+      case '/dashboard/teams-info':
+        this.name = 'Equipos';
+        break;
+      case '/dashboard/players':
+        this.name = 'Jugadores';
+        break;
+      default:
+        this.name = 'Campus League';
+    }
+  }
   toggleDropdown(dropdown: string) {
     this.activeDropdown = this.activeDropdown === dropdown ? null : dropdown;
   }
